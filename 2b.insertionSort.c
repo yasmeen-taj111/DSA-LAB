9ac1f2840b8ec2d72db1d2c52b47cd2f1d664f5a
Yasmeen Taj <yasmeentaj.6041@gmail.com>
2025-11-11 20:13:17 +0530

    program to implement insertion sort:

---
+++ 2b.insertionSort.c
@@ -0,0 +1,32 @@
+//2 b. Write a program to implement insertion sort:
+
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int a[20], n, i, j, key;
+
+    clrscr();
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 1; i < n; i++) {
+        key = a[i];
+        j = i - 1;
+        while(j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+
+    printf("After sorting:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    getch();
+}
