146bb81f31a81f64feb18c832b79afc4bcabeb7f
Yasmeen Taj <yasmeentaj.6041@gmail.com>
2025-11-15 23:37:26 +0530

    Evaluation expression tree using binary tree.
    Implement evaluation of expression tree from postfix notation.

---
+++ expressionTree.c
@@ -0,0 +1,71 @@
+//8. Evaluation expression tree using binary tree
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+struct node {
+    char data;
+    struct node *lchild, *rchild;
+};
+typedef struct node* NODE;
+
+NODE create_tree(char postfix[]){
+     NODE stack[20], temp;
+    int i, top = -1;
+
+    for(i = 0; postfix[i] != '\0'; i++) {
+        temp = (NODE)malloc(sizeof(struct node));
+        temp->data = postfix[i];
+        temp->lchild = temp->rchild = NULL;
+
+        if(isalnum(postfix[i])) {
+            stack[++top] = temp;
+        } else {
+            temp->rchild = stack[top--];
+            temp->lchild = stack[top--];
+            stack[++top] = temp;
+        }
+    }
+    return stack[top];
+    
+}
+float eval(NODE root){
+    float num;
+
+    if(isdigit(root->data))
+        return (root->data - '0');
+
+    if(isalpha(root->data)) {
+        printf("Enter value of %c: ", root->data);
+        scanf("%f", &num);
+        return num;
+    }
+
+    switch(root->data) {
+        case '+': return eval(root->lchild) + eval(root->rchild);
+        case '-': return eval(root->lchild) - eval(root->rchild);
+        case '*': return eval(root->lchild) * eval(root->rchild);
+        case '/': return eval(root->lchild) / eval(root->rchild);
+        case '^': return pow(eval(root->lchild), eval(root->rchild));
+    }
+    return 0;
+    
+}
+
+int main() {
+    char postfix[20];
+    float result;
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+
+    NODE root = create_tree(postfix);
+    result = eval(root);
+
+    printf("Result = %f\n", result);
+    return 0;
+}
+
+
